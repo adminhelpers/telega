b63edf5529c7a4849e83251867d4c8b586bb8693
adminhelpers <75928968+adminhelpers@users.noreply.github.com>
2022-07-27 16:03:22 +0800

    Update recrute.py

--- recrute.py
+++ recrute.py
@@ -1,6 +1,5 @@
 import os
 
 if __name__ == '__main__':
-    files = ["time_rise.py", "main.py"]  # файлы, которые нужно запустить
-    for file in files:
-        os.system(f'python {file}')
+    os.system('python time_rise.py')
+    os.system('python main.py')
